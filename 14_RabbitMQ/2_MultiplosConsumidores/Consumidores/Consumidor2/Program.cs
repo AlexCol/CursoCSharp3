a54dae9f9960f33da26d018a92d50f81a4a4237a
Alexandre B Coletti <alexandre.coletti@controlsoft.com.br>
2024-10-08 13:06:24 -0300

    .

---
+++ 14_RabbitMQ/2_MultiplosConsumidores/Consumidores/Consumidor2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// Configura a fábrica de conexão com RabbitMQ, especificando o servidor e credenciais
+var servidor = new ConnectionFactory {
+    HostName = "localhost", // Endereço do servidor RabbitMQ
+    Port = 5672,            // Porta padrão do RabbitMQ
+    UserName = "usuario",   // Nome de usuário para autenticação
+    Password = "Senha@123"  // Senha de acesso ao servidor
+};
+
+// Cria a conexão usando as configurações definidas acima
+var conexao = servidor.CreateConnection();
+
+// Com a conexão ativa, abre um canal de comunicação com o servidor
+using var canal = conexao.CreateModel();
+
+// Declara a fila de mensagens (queue) no servidor, garantindo que ela exista
+// - durable: a fila persiste entre reinicializações do servidor
+// - exclusive: false permite que outros canais usem a mesma fila
+// - autoDelete: false impede a exclusão automática da fila quando não estiver em uso
+canal.QueueDeclare(
+    queue: "fila_de_tarefas",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+// Define que o canal deve receber uma mensagem por vez para processar (limita a carga de processamento)
+canal.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+Console.WriteLine(" [*] Aguardando mensagens...");
+
+// Cria o consumidor de eventos que processará as mensagens recebidas da fila
+var consumidor = new EventingBasicConsumer(canal);
+
+// Define o evento de recebimento de mensagem para o consumidor
+consumidor.Received += (sender, evento) => {
+    // Extrai o corpo da mensagem e converte de byte array para string
+    try {
+        //throw new Exception("ops");
+        var corpoMensagem = evento.Body.ToArray();
+        var mensagem = Encoding.UTF8.GetString(corpoMensagem);
+
+        // Exibe a mensagem recebida no console
+        Console.WriteLine(" [x] Mensagem recebida: {0}", mensagem);
+
+        // Confirma o processamento da mensagem para o RabbitMQ (envia um ACK)
+        canal.BasicAck(evento.DeliveryTag, multiple: false);
+    } catch (Exception ex) {
+        Console.WriteLine("Erro ao processar a mensagem: {0}", ex.Message);
+        canal.BasicNack(evento.DeliveryTag, multiple: false, requeue: true); //com erro, mando a mensagem de volta para a fila
+    }
+};
+
+// Inicia o consumo de mensagens da fila, usando o consumidor configurado
+// - autoAck: false indica que a confirmação de processamento será manual (via BasicAck)
+canal.BasicConsume(
+    queue: "fila_de_tarefas",
+    autoAck: false,
+    consumer: consumidor
+);
+
+// Mantém o console aberto até o usuário pressionar "Enter"
+Console.WriteLine(" Pressione [enter] para sair.");
+Console.ReadLine();
---
+++ 14_RabbitMQ/2_MultiplosConsumidores/Publicador/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+// Criação da conexão com o servidor RabbitMQ
+var server = new ConnectionFactory() {
+    // Configurações do servidor RabbitMQ, incluindo hostname, porta e credenciais
+    HostName = "localhost",      // Endereço do servidor (localhost para ambiente local)
+    Port = 5672,                 // Porta padrão do RabbitMQ (5672)
+    UserName = "usuario",        // Usuário de autenticação
+    Password = "Senha@123"       // Senha de autenticação
+};
+
+// Estabelece a conexão com o servidor RabbitMQ
+using var conexao = server.CreateConnection();
+
+// Criação de um canal de comunicação com o RabbitMQ
+using var canal = conexao.CreateModel();
+
+// Declara a fila chamada "fila_01" para garantir que existe antes de enviar mensagens
+canal.QueueDeclare(
+    queue: "fila_de_tarefas",    // Nome da fila
+    durable: true,               // 'true' indica que a fila persiste após reiniciar o servidor
+    exclusive: false,            // 'false' permite que outros canais acessem a fila
+    autoDelete: false,           // 'false' indica que a fila não será apagada quando não estiver em uso
+    arguments: null              // Argumentos adicionais (null, neste caso)
+);
+
+// Define a mensagem a ser enviada
+Console.WriteLine("Digite uma mensagem (ou '!finalizar' para encerrar):");
+while (true) {
+    string? mensagem = Console.ReadLine();
+    if (mensagem == null || mensagem == "!finalizar") break;
+
+    // Converte a string da mensagem para um array de bytes para envio pelo RabbitMQ
+    var corpoMensagem = Encoding.UTF8.GetBytes(mensagem!);
+
+    // Publica a mensagem no RabbitMQ na fila especificada
+    canal.BasicPublish(
+        exchange: "",                    // Exchange vazio usa o "default exchange"
+        routingKey: "fila_de_tarefas",   // Nome da fila como chave de roteamento
+        basicProperties: null,           // Propriedades adicionais (null neste caso)
+        body: corpoMensagem              // Corpo da mensagem em bytes
+    );
+
+    // Log de confirmação de envio
+    Console.WriteLine("[x] Mensagem enviada: {0}", mensagem);
+}
+
+// Aguardar o usuário pressionar uma tecla para encerrar
+Console.WriteLine("Pressione qualquer tecla para sair");
+Console.ReadKey();
