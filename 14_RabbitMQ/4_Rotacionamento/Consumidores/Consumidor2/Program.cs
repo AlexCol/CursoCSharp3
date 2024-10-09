dce1531057f3313ee31659053535a5eb6a7c72fc
Alexandre B Coletti <alexandre.coletti@controlsoft.com.br>
2024-10-09 13:04:29 -0300

    .

---
+++ 14_RabbitMQ/4_Rotacionamento/Consumidores/Consumidor2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// Configura a conexão com o servidor RabbitMQ
+var servidor = new ConnectionFactory() {
+    HostName = "localhost",        // Endereço do servidor RabbitMQ (neste caso, local)
+    Port = 5672,                   // Porta padrão do RabbitMQ
+    UserName = "usuario",          // Nome de usuário para autenticação
+    Password = "Senha@123"         // Senha para autenticação
+};
+
+// Cria uma conexão com o servidor RabbitMQ
+var conexao = servidor.CreateConnection();
+
+// Cria um canal de comunicação dentro da conexão
+using var canal = conexao.CreateModel();
+
+// Declara uma exchange chamada "mensagem_empresa" do tipo "direct",
+// que permite o envio de mensagens para filas específicas com base
+// em uma chave de roteamento (routing key).
+canal.ExchangeDeclare(
+    exchange: "mensagem_empresa",
+    type: ExchangeType.Direct
+);
+
+// Declara uma fila exclusiva com um nome gerado automaticamente pelo RabbitMQ
+// para receber as mensagens direcionadas a este consumidor
+//var nomeFila = canal.QueueDeclare().QueueName; //para criar uma fila com nome aleatório
+
+var nomeFila = "fila_compras"; //para criar uma fila com nome específico
+canal.QueueDeclare(
+    queue: "fila_compras",  // Nome da fila, neste caso "fila_vendas"
+    durable: true,         // Define a durabilidade da fila, ou seja, se ela sobreviverá a reinicializações do servidor
+    exclusive: false,      // Define se a fila é exclusiva ao consumidor que a criou
+    autoDelete: false,     // Define se a fila será excluída automaticamente quando todos os consumidores forem desconectados
+    arguments: null        // Argumentos adicionais para configurações avançadas
+);
+
+// Liga a fila declarada à exchange "mensagem_empresa" com a chave de roteamento "Compras".
+// Assim, a fila receberá apenas as mensagens enviadas com essa rota específica.
+canal.QueueBind(
+    queue: nomeFila,
+    exchange: "mensagem_empresa",
+    routingKey: "Compras"
+);
+
+Console.WriteLine("Consumidor 2 - Aguardando mensagens...");
+
+// Cria um consumidor para processar mensagens da fila
+var consumidor = new EventingBasicConsumer(canal);
+
+// Define o que fazer ao receber uma mensagem
+consumidor.Received += (sender, evento) => {
+    // Obtém o conteúdo da mensagem em bytes
+    var corpoMensagem = evento.Body.ToArray();
+    // Converte o conteúdo da mensagem para uma string
+    var mensagem = Encoding.UTF8.GetString(corpoMensagem);
+
+    // Exibe a mensagem recebida no console
+    Console.WriteLine("[x] Mensagem recebida: {0}", mensagem);
+};
+
+// Inicia o consumo de mensagens na fila especificada
+// autoAck: true significa que o RabbitMQ marcará a mensagem como confirmada automaticamente após o consumo
+canal.BasicConsume(queue: nomeFila, autoAck: true, consumer: consumidor);
+
+Console.WriteLine("Pressione [enter] para finalizar.");
+Console.ReadLine(); // Aguarda o usuário pressionar Enter para encerrar o programa
---
+++ 14_RabbitMQ/4_Rotacionamento/Publicador/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+// Configura a conexão com o servidor RabbitMQ
+var servidor = new ConnectionFactory() {
+  HostName = "localhost",         // Endereço do servidor RabbitMQ (neste caso, local)
+  Port = 5672,                    // Porta padrão do RabbitMQ
+  UserName = "usuario",           // Nome de usuário para autenticação
+  Password = "Senha@123"          // Senha para autenticação
+};
+
+// Cria uma conexão com o servidor RabbitMQ
+var conexao = servidor.CreateConnection();
+
+// Cria um canal de comunicação dentro da conexão
+using var canal = conexao.CreateModel();
+
+// Declara uma exchange chamada "mensagem_empresa" do tipo "direct",
+// que permite o envio de mensagens para filas específicas com base
+// em uma chave de roteamento (routing key).
+canal.ExchangeDeclare(
+    exchange: "mensagem_empresa",
+    type: ExchangeType.Direct
+);
+
+Console.WriteLine("Digite uma mensagem (ou '!finalizar' para encerrar):");
+
+// Loop que permite o envio de mensagens contínuas
+while (true) {
+  // Lê a mensagem do usuário
+  string? mensagem = Console.ReadLine();
+
+  // Verifica se o usuário deseja encerrar o programa
+  if (mensagem == null || mensagem == "!finalizar") break;
+
+  // Converte a mensagem para um array de bytes para o envio
+  var corpoMensagem = Encoding.UTF8.GetBytes(mensagem!);
+
+  Console.WriteLine("Digite uma rota:");
+  // Lê a chave de roteamento fornecida pelo usuário
+  string? rota = Console.ReadLine();
+
+  // Publica a mensagem na exchange "mensagem_empresa" com a chave de roteamento especificada
+  canal.BasicPublish(
+      exchange: "mensagem_empresa", // Nome da exchange onde a mensagem será enviada
+      routingKey: rota,             // Rota (chave de roteamento) que especifica para onde a mensagem deve ser direcionada
+      basicProperties: null,        // Propriedades da mensagem (neste caso, null)
+      body: corpoMensagem           // Conteúdo da mensagem em bytes
+  );
+
+  Console.WriteLine("[x] Mensagem enviada: {0}", mensagem);
+}
+
+Console.WriteLine("Pressione qualquer tecla para sair");
+Console.ReadKey(); // Aguarda o usuário pressionar uma tecla para encerrar o programa
